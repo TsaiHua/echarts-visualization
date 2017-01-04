4b1d4682dea51d6375f68c75f0442dfbd95ae670
nowToTsai3 <cwchyj@139.com>
2017-01-04 14:10:35 +0800

    tunmo frame established.

--- src/home/controller/tunmo.js
+++ src/home/controller/tunmo.js
@@ -7,10 +7,27 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  indexAction(){
-    //auto render template file index_index.html
-    this.assign("title", "吞没指标");
-    this.assign("tunmo", "active");
+  d1Action(){
+    this.assign("title", "吞没(1天)");
+    this.assign("tunmod1", "active");
+    return this.display();
+  }
+  /**
+   * gbpusd action
+   * @return {Promise} []
+   */
+  h4Action(){
+    this.assign("title", "吞没(4小时)");
+    this.assign("tunmoh4", "active");
+    return this.display();
+  }
+  /**
+   * eurusd action
+   * @return {Promise} []
+   */
+  h1Action(){
+    this.assign("title", "吞没(1小时)");
+    this.assign("tunmoh1", "active");
     return this.display();
   }
 }
